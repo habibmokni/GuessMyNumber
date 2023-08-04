378c826ffdb56dac57bda3f8faac0f6950877457
habibmokni <habib.mokni@outlook.fr>
2023-08-04 22:52:04 +0200

    Implement highscore

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 // const message = document.querySelector('.message').textContent;
 // const scoreValue = document.querySelector('.score').textContent;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.again').addEventListener('click', () => {
   secretNumber = Math.trunc(Math.random() * 20) + 1;
@@ -27,6 +28,11 @@ document.querySelector('.check').addEventListener('click', () => {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     // where guess is too high
   } else if (guess > secretNumber) {
