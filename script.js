a1fe961ea24a3ae741f053ec759245118c824346
habibmokni <habib.mokni@outlook.fr>
2023-08-04 22:23:56 +0200

    Manipulating styling

--- script.js
+++ script.js
@@ -10,10 +10,17 @@ document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
   //   console.log(guess, typeof guess);
 
+  // where there is no input
   if (!guess) {
     document.querySelector('.message').textContent = 'Not a Number!';
+    // where player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    // where guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Guess is high!';
@@ -23,6 +30,7 @@ document.querySelector('.check').addEventListener('click', () => {
       document.querySelector('.message').textContent = 'You lost the game!';
       document.querySelector('.score').textContent = 0;
     }
+    // where guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Guess is low!';
